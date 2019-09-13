49d7d4e72f084f43374054ec8f09fba818097776
dennis1a4 <dennis1a4@yahoo.ca>
2019-09-13 14:54:16 -0500

    Update LSM303AGR_U.h
    Setup mag gain for AGR.

--- LSM303AGR_U.h
+++ LSM303AGR_U.h
@@ -103,18 +103,13 @@
 /*=========================================================================*/
 
 /*=========================================================================
-    MAGNETOMETER GAIN SETTINGS
+    TEMPERATURE CONGIF SETTINGS
     -----------------------------------------------------------------------*/
     typedef enum
     {
-      LSM303_MAGGAIN_1_3                        = 0x20,  // +/- 1.3
-      LSM303_MAGGAIN_1_9                        = 0x40,  // +/- 1.9
-      LSM303_MAGGAIN_2_5                        = 0x60,  // +/- 2.5
-      LSM303_MAGGAIN_4_0                        = 0x80,  // +/- 4.0
-      LSM303_MAGGAIN_4_7                        = 0xA0,  // +/- 4.7
-      LSM303_MAGGAIN_5_6                        = 0xC0,  // +/- 5.6
-      LSM303_MAGGAIN_8_1                        = 0xE0   // +/- 8.1
-    } lsm303MagGain;
+      LSM303_TEMP_ON                            = 0xC0,  //
+      LSM303_TEMP_OFF                           = 0x00   //
+    } lsm303TempCfg;
 /*=========================================================================*/
 
 /*=========================================================================
@@ -122,14 +117,10 @@
     -----------------------------------------------------------------------*/
     typedef enum
     {
-      LSM303_MAGRATE_0_7                        = 0x00,  // 0.75 Hz
-      LSM303_MAGRATE_1_5                        = 0x01,  // 1.5 Hz
-      LSM303_MAGRATE_3_0                        = 0x62,  // 3.0 Hz
-      LSM303_MAGRATE_7_5                        = 0x03,  // 7.5 Hz
-      LSM303_MAGRATE_15                         = 0x04,  // 15 Hz
-      LSM303_MAGRATE_30                         = 0x05,  // 30 Hz
-      LSM303_MAGRATE_75                         = 0x06,  // 75 Hz
-      LSM303_MAGRATE_220                        = 0x07   // 200 Hz
+      LSM303_MAGRATE_10                         = 0x00,  // 10 Hz
+      LSM303_MAGRATE_20                         = 0x04,  // 20 Hz
+      LSM303_MAGRATE_50                         = 0x08,  // 50 Hz
+      LSM303_MAGRATE_100                        = 0x0C   // 100 Hz
     } lsm303MagRate;
 /*=========================================================================*/
 
@@ -153,12 +144,6 @@
       int16_t y;
       int16_t z;
     } lsm303AccelData;
-/*=========================================================================*/
-
-/*=========================================================================
-    CHIP ID
-    -----------------------------------------------------------------------*/
-    #define LSM303_ID                     (0b11010100)
 /*=========================================================================*/
 
 /* Unified sensor driver for the accelerometer */
@@ -189,14 +174,11 @@ class Adafruit_LSM303_Mag_Unified : public Adafruit_Sensor
 
     bool begin(void);
     void enableAutoRange(bool enable);
-    void setMagGain(lsm303MagGain gain);
     void setMagRate(lsm303MagRate rate);
     bool getEvent(sensors_event_t*);
     void getSensor(sensor_t*);
 
     lsm303MagData   raw;     // Last read magnetometer data will be available here
-    lsm303MagGain   magGain;
-    bool            autoRangeEnabled;
 
   private:
     int32_t         _sensorID;
@@ -212,7 +194,6 @@ class Adafruit_LSM303
   public:
     bool begin(void);
     void read(void);
-    void setMagGain(lsm303MagGain gain);
 
     lsm303AccelData accelData;    // Last read accelerometer data will be available here
     lsm303MagData magData;        // Last read magnetometer data will be available here
