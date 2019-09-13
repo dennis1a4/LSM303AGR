809d8583ffd5cef194941b93d759fbcc713ab826
dennis1a4 <dennis1a4@yahoo.ca>
2019-09-13 14:19:26 -0500

    Update and rename Adafruit_LSM303_U.h to LSM303AGR_U.h
    Added LSM303AGR registers.

--- LSM303AGR_U.h
+++ LSM303AGR_U.h
@@ -1,9 +1,9 @@
 /***************************************************************************
   This is a library for the LSM303 Accelerometer and magnentometer/compass
 
-  Designed specifically to work with the Adafruit LSM303DLHC Breakout
+  Designed specifically to work with the Adafruit LSM303AGR
 
-  These displays use I2C to communicate, 2 pins are required to interface.
+  These devices use I2C to communicate, 2 pins are required to interface.
 
   Adafruit invests time and resources providing this open source code,
   please support Adafruit andopen-source hardware by purchasing products
@@ -12,8 +12,8 @@
   Written by Kevin Townsend for Adafruit Industries.
   BSD license, all text above must be included in any redistribution
  ***************************************************************************/
-#ifndef __LSM303_H__
-#define __LSM303_H__
+#ifndef __LSM303AGR_H__
+#define __LSM303AGR_H__
 
 #if (ARDUINO >= 100)
  #include "Arduino.h"
@@ -21,7 +21,6 @@
  #include "WProgram.h"
 #endif
 
-#include <Adafruit_Sensor.h>
 #include <Wire.h>
 
 
@@ -37,6 +36,12 @@
     -----------------------------------------------------------------------*/
     typedef enum
     {                                                     // DEFAULT    TYPE
+      LSM303_REGISTER_ACCEL_STATUS_REG_AUX_A    = 0x07,   //            r   Temp overrun/new data
+      LSM303_REGISTER_ACCEL_OUT_TEMP_L_A        = 0x0C,   //            r   Temp output low
+      LSM303_REGISTER_ACCEL_OUT_TEMP_H_A        = 0x0D,   //            r   Temp output high
+      LSM303_REGISTER_ACCEL_INT_COUNTER_REG_A   = 0x0E,   //            r
+      LSM303_REGISTER_ACCEL_WHO_AM_I_A          = 0x0F,   // 00110011   r
+      LSM303_REGISTER_ACCEL_TEMP_CFG_REG_A      = 0x1C,   // 00000000   rw  Temp config reg
       LSM303_REGISTER_ACCEL_CTRL_REG1_A         = 0x20,   // 00000111   rw
       LSM303_REGISTER_ACCEL_CTRL_REG2_A         = 0x21,   // 00000000   rw
       LSM303_REGISTER_ACCEL_CTRL_REG3_A         = 0x22,   // 00000000   rw
@@ -66,26 +71,34 @@
       LSM303_REGISTER_ACCEL_CLICK_THS_A         = 0x3A,
       LSM303_REGISTER_ACCEL_TIME_LIMIT_A        = 0x3B,
       LSM303_REGISTER_ACCEL_TIME_LATENCY_A      = 0x3C,
-      LSM303_REGISTER_ACCEL_TIME_WINDOW_A       = 0x3D
+      LSM303_REGISTER_ACCEL_TIME_WINDOW_A       = 0x3D,
+      LSM303_REGISTER_ACCEL_Act_THS_A           = 0x3E,   // 00000000   rw Sleep-to-wake, return-to-sleep activation threshold in low-power mode
+      LSM303_REGISTER_ACCEL_Act_DUR_A           = 0x3F    // 00000000   rw Sleep-to-wake, return-to-sleep duration
     } lsm303AccelRegisters_t;
 
     typedef enum
     {
-      LSM303_REGISTER_MAG_CRA_REG_M             = 0x00,
-      LSM303_REGISTER_MAG_CRB_REG_M             = 0x01,
-      LSM303_REGISTER_MAG_MR_REG_M              = 0x02,
-      LSM303_REGISTER_MAG_OUT_X_H_M             = 0x03,
-      LSM303_REGISTER_MAG_OUT_X_L_M             = 0x04,
-      LSM303_REGISTER_MAG_OUT_Z_H_M             = 0x05,
-      LSM303_REGISTER_MAG_OUT_Z_L_M             = 0x06,
-      LSM303_REGISTER_MAG_OUT_Y_H_M             = 0x07,
-      LSM303_REGISTER_MAG_OUT_Y_L_M             = 0x08,
-      LSM303_REGISTER_MAG_SR_REG_Mg             = 0x09,
-      LSM303_REGISTER_MAG_IRA_REG_M             = 0x0A,
-      LSM303_REGISTER_MAG_IRB_REG_M             = 0x0B,
-      LSM303_REGISTER_MAG_IRC_REG_M             = 0x0C,
-      LSM303_REGISTER_MAG_TEMP_OUT_H_M          = 0x31,
-      LSM303_REGISTER_MAG_TEMP_OUT_L_M          = 0x32
+      LSM303_REGISTER_MAG_OFFSET_X_REG_L_M      = 0x45,   //            rw
+      LSM303_REGISTER_MAG_OFFSET_X_REG_H_M      = 0x46,   //            rw
+      LSM303_REGISTER_MAG_OFFSET_Y_REG_L_M      = 0x47,   //            rw
+      LSM303_REGISTER_MAG_OFFSET_Y_REG_H_M      = 0x48,   //            rw
+      LSM303_REGISTER_MAG_OFFSET_Z_REG_L_M      = 0x49,   //            rw
+      LSM303_REGISTER_MAG_OFFSET_Z_REG_H_M      = 0x4A,   //            rw
+      LSM303_REGISTER_MAG_WHO_AM_I_M            = 0x4F,   //            r
+      LSM303_REGISTER_MAG_CFG_REG_A_M           = 0x60,   // 00000011   rw
+      LSM303_REGISTER_MAG_CFG_REG_B_M           = 0x61,   //            rw
+      LSM303_REGISTER_MAG_CFG_REG_C_M           = 0x62,   //            rw
+      LSM303_REGISTER_MAG_INT_CTRL_REG_M        = 0x63,   // 11100000   rw
+      LSM303_REGISTER_MAG_INT_SOURCE_REG_M      = 0x64,   //            r
+      LSM303_REGISTER_MAG_INT_THS_L_REG_M       = 0x65,   //            rw
+      LSM303_REGISTER_MAG_INT_THS_H_REG_M       = 0x66,   //            rw
+      LSM303_REGISTER_MAG_STATUS_REG_M          = 0x67,   //            r
+      LSM303_REGISTER_MAG_OUTX_L_REG_M          = 0x68,   //            r
+      LSM303_REGISTER_MAG_OUTX_H_REG_M          = 0x69,   //            r
+      LSM303_REGISTER_MAG_OUTY_L_REG_M          = 0x6A,   //            r
+      LSM303_REGISTER_MAG_OUTY_H_REG_M          = 0x6B,   //            r
+      LSM303_REGISTER_MAG_OUTZ_L_REG_M          = 0x6C,   //            r
+      LSM303_REGISTER_MAG_OUTZ_H_REG_M          = 0x6D    //            r
     } lsm303MagRegisters_t;
 /*=========================================================================*/
 
