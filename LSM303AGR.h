09862eb59fb7e91629b8cfbe4761f31d95a25860
dennis1a4 <dennis1a4@yahoo.ca>
2019-09-16 09:10:19 -0500

    Update LSM303AGR.h

--- LSM303AGR.h
+++ LSM303AGR.h
@@ -1,6 +1,6 @@
 // This file is provided for compatibility reasons with the deprecated
-// (non-Unified) Adafruit_LSM303 library. Any new development should use
-// the Adafruit_LSM303_U.h header, this file is provided to try to minimize
+// (non-Unified) LSM303AGR library. Any new development should use
+// the LSM303AGR_U.h header, this file is provided to try to minimize
 // issues with projects that depend on the older driver.
 
 #ifndef __LSM303AGR_H__
